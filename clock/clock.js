7507d44f7e76aabf4b192109d1b58283fcbbe36d
PASSERON Jeremy <jpasseron@gmail.com>
2017-04-06 13:32:14 +0200

    clock is complete

--- clock/clock.js
+++ clock/clock.js
@@ -4,71 +4,58 @@ var Clock = function(h, m) {
     this.originalH = h ;
     this.originalM = m ;
 
-    console.log("\n------")
-    console.log("h="+h+"  m="+m)
-    /* m is not mandatory */
+    this.hours = 0 ;
+    this.minutes = 0 ; 
+
     if(isNaN(m)){m=0}    
 
-    /* negative hours or minutes */
-    h = fixHours(h)        
-    /* lets work on the hours */
-    let hours = h + gimmeNbHours(m);
-    console.log("hours="+hours)
-    if(hours>24){
-        hours = Math.floor(hours % 24)
-    }if(hours<0){
-        hours = 24 + hours
-    }
-    /* hour 24 is a special case */
-    if(hours==24){
-        hours=0
-    }
-    hours = fixHours(hours)            
-    let minutes = fixMinutes(m) ;
-
-
-    this.hours = hours 
-    this.minutes = minutes
+    // set the minut field
+    this.minutes = this.computeMinutes(m)
+    // set the hour field : add hours if minutes absolute value is > 60
+    this.hours = this.computeHours(h + this.computeHoursOverFlowFromMinutes(m))
 }
 
-Clock.at = function(h,m){
-    
+Clock.at = function(h,m){    
     return new Clock(h, m)
 }
 
+Clock.prototype.computeHours = function(h){
+    var res = h % 24 ;
+    if(res>=0){
+        return res
+    }else{        
+        return Math.abs((Math.floor(res/24) * 24) - res)
+    }
+}
+
+Clock.prototype.computeMinutes = function(m){
+    var res =  m % 60 ;
+    if(res >= 0){
+        return res 
+    }else{
+        return Math.abs((Math.floor(res/60) * 60) - res)
+    }
+
+}
+
+Clock.prototype.computeHoursOverFlowFromMinutes = function(m){
+    return Math.floor(m/60)
+}
+
 Clock.prototype.plus = function(nbMinutes){
-    return new Clock(this.originalH, this.originalM+nbMinutes)
+    return new Clock(this.originalH, this.originalM + nbMinutes)
+}
+
+Clock.prototype.minus = function(nbMinutes){
+    return new Clock(this.originalH, this.originalM - nbMinutes)
 }
 
 Clock.prototype.toString = function(){
     return ('0' + this.hours).slice(-2)+':'+('0' + this.minutes).slice(-2)
 }
 
-Clock.prototype.equals = function(anotherClock){
-    return this.hour == anotherClock.hour && this.minutes == anotherClock.minutes
-}
-
-fixHours = function(hours){
-    if(Math.abs(hours)>=24){
-        hours = Math.floor(hours % 24)
-    }
-    if(hours<0){
-        hours = 24 + hours    
-    }
-    return hours
-}
-
-fixMinutes = function(minutes){
-    var res = minutes % 60 ;
-    if(res<0){
-        return 60 + res
-    }else{
-        return res
-    }
-}
-
-gimmeNbHours = function(minutes){    
-    return Math.floor(minutes/60)
+Clock.prototype.equals = function(anotherClock){    
+    return this.hours === anotherClock.hours && this.minutes === anotherClock.minutes
 }
 
 module.exports = Clock;
