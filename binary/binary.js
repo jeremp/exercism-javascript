eec6abced88a2c0e38a2488e8102e4ce5741566b
PASSERON Jeremy <jpasseron@gmail.com>
2017-05-19 16:03:41 +0200

    stored test

---
+++ binary/binary.js
@@ -0,0 +1,13 @@
+var Binary = function(word){
+    this.word = word ;    
+};
+
+Binary.prototype.toDecimal = function(decimalString){
+    var array  = decimalString.split("");
+}
+
+var splitData = function(){
+
+}
+
+module.exports = Binary;
